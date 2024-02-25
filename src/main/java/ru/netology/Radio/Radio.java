fd0686201b0982b61d5261d0b6d5102fdf3a9857
Anna Evseykina <anytta9032@yandex.ru>
2024-02-25 19:51:38 +0700

    add Radio and RadioTest

--- src/main/java/ru/netology/Radio/Radio.java
+++ src/main/java/ru/netology/Radio/Radio.java
@@ -1,14 +1,58 @@
 package ru.netology.Radio;
 
 public class Radio {
-
     public int stationNumber;
 
     public void stationNumberMax() {
         stationNumber = 9;
+    }
 
+    public int getStationNumber() {
+        return stationNumber;
+    }
+
+    public void setStationNumber(int newStationNumber) {
+        if (newStationNumber < 0) {
+            return;
+        }
+        if (newStationNumber > 9) {
+            newStationNumber = newStationNumber + 1;
+            {
+                return;
+            }
+        }
+        stationNumber = newStationNumber;
+    }
+
+    public int Volume;
+
+    public void VolumeMax() {
+        Volume = 100;
+    }
+
+    public int getVolume() {
+        return Volume;
+    }
+
+    public void setVolume(int newVolume) {
+        if (newVolume > 100) {
+            return;
+        }
+        if (newVolume < 0) {
+            return;
+        }
+        if (newVolume < 100) {
+            newVolume = newVolume + 1;
+            {
+                return;
+            }
+        }
+        Volume = newVolume;
     }
 }
+
+
+
 
 
     
--- src/test/java/ru/netology/Radio/RadioTest.java
+++ src/test/java/ru/netology/Radio/RadioTest.java
@@ -3,23 +3,60 @@ package ru.netology.Radio;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class RadioTest {
     @Test
     public void shouldSetStationNumber() {
         Radio radio = new Radio();
-        radio.stationNumber = 5;
+        radio.setStationNumber(5);
         int expected = 5;
-        int actual = radio.stationNumber;
+        int actual = radio.getStationNumber();
         Assertions.assertEquals(expected, actual);
     }
     @Test
     public void shouldStationNumberMax() {
         Radio radio = new Radio();
-        radio.stationNumberMax();
-         int expected = 9;
-        int actual = radio.stationNumber;
-        Assertions.assertEquals(expected,actual);
+        radio.setStationNumber(9);
+        int expected = 9;
+        int actual = radio.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldNotStationNumberMax() {
+        Radio radio = new Radio();
+        radio.setStationNumber(15);
+        int expected = 0;
+        int actual = radio.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(50);
+        int expected = 50;
+        int actual = radio.getVolume();
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldNotVolumeMax() {
+        Radio radio = new Radio();
+        radio.setVolume(105);
+        int expected = 0;
+        int actual = radio.getVolume();
+        Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldVolumeMin0() {
+        Radio radio = new Radio();
+        radio.setVolume(-5);
+        int expected = 0;
+        int actual = radio.getVolume();
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
+
+
+
+
